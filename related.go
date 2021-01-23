011336de883e44b2ae13b2d0563680a7a3b6991c
Konstantinos Pittas <konstantinos.pittas@gmail.com>
2021-01-23 14:48:17 +0200

    fix const names in comments

--- related.go
+++ related.go
@@ -1,46 +1,46 @@
 package datamuse
 
-// Nouns - Popular nouns modified by the given adjective, per Google Books Ngrams
+// RelatedNouns - Popular nouns modified by the given adjective, per Google Books Ngrams
 const RelatedNouns string = "jja"
 
-// Adjectives - Popular adjectives used to modify the given noun, per Google Books Ngrams
+// RelatedAdjectives - Popular adjectives used to modify the given noun, per Google Books Ngrams
 const RelatedAdjectives string = "jjb"
 
-// Synonyms (words contained within the same WordNet synset)
+// RelatedSynonyms (words contained within the same WordNet synset)
 const RelatedSynonyms string = "syn"
 
-// Triggers (words that are statistically associated with the query word in the same piece of text.)
+// RelatedTriggers (words that are statistically associated with the query word in the same piece of text.)
 const RelatedTriggers string = "trg"
 
-// Antonyms (per WordNet)
+// RelatedAntonyms (per WordNet)
 const RelatedAntonyms string = "ant"
 
-// KindOf (direct hypernyms, per WordNet)
+// RelatedKindOf (direct hypernyms, per WordNet)
 const RelatedKindOf string = "spc"
 
-// MoreGeneralThan (direct hyponyms, per WordNet)
+// RelatedMoreGeneralThan (direct hyponyms, per WordNet)
 const RelatedMoreGeneralThan string = "gen"
 
-// Comprises (direct holonyms, per WordNet)
+// RelatedComprises (direct holonyms, per WordNet)
 const RelatedComprises string = "com"
 
-// PartOf (direct meronyms, per WordNet)
+// RelatedPartOf (direct meronyms, per WordNet)
 const RelatedPartOf string = "par"
 
-// FrequentFollowers (w′ such that P(w′|w) ≥ 0.001, per Google Books Ngrams)
+// RelatedFrequentFollowers (w′ such that P(w′|w) ≥ 0.001, per Google Books Ngrams)
 const RelatedFrequentFollowers string = "bga"
 
-// FrequentPredecessors (w′ such that P(w|w′) ≥ 0.001, per Google Books Ngrams)
+// RelatedFrequentPredecessors (w′ such that P(w|w′) ≥ 0.001, per Google Books Ngrams)
 const RelatedFrequentPredecessors string = "bgb"
 
-// Rhymes ("perfect" rhymes, per RhymeZone)
+// RelatedRhymes ("perfect" rhymes, per RhymeZone)
 const RelatedRhymes string = "rhy"
 
-// ApproximateRhymes (per RhymeZone)
+// RelatedApproximateRhymes (per RhymeZone)
 const RelatedApproximateRhymes string = "nry"
 
-// Homophones (sound-alike words)
+// RelatedHomophones (sound-alike words)
 const RelatedHomophones string = "hom"
 
-// Consonant match
+// RelatedConsonant match
 const RelatedConsonant string = "cns"
--- vocabularies.go
+++ vocabularies.go
@@ -1,11 +1,11 @@
 package datamuse
 
-// Default is a 550,000-term vocabulary of English words and multiword expressions.
+// VocabularyDefault is a 550,000-term vocabulary of English words and multiword expressions.
 const VocabularyDefault = ""
 
-// Spanish is a 500,000-term vocabulary of words from Spanish-language books.
+// VocabularySpanish is a 500,000-term vocabulary of words from Spanish-language books.
 const VocabularySpanish = "es"
 
-// Wikipedia is an approximately 6 million-term vocabulary of article titles from the English-language Wikipedia
+// VocabularyWikipedia is an approximately 6 million-term vocabulary of article titles from the English-language Wikipedia
 // (updated monthly).
 const VocabularyWikipedia = "enwiki"
